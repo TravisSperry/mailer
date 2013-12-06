e67518852a4a461aba5d731e1b3af9504e41f2e9
tkendalls <director@mathplusacademy.com>
2013-12-06 09:46:49 -0500

    mailer | http://drone.bz

---
+++ app/assets/javascripts/application.js.coffee
@@ -0,0 +1,10 @@
+//= require jquery
+//= require jquery_ujs
+//= require init
+//= require jquery.html5-placeholder-shim
+//= require underscore
+//= require gritter
+//= require chosen-jquery
+//= require jquery.remotipart
+//= require jquery_nested_form
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailer::Application.config.secret_token = '6592221d5c3aad26b08fbd3fbca82512b4e113df1c7e084876f68a588b6a1958507dabcdcf1698ba019170c3a83fee9f43ee0c7b371adb0d068b596a10ca1bfc'
