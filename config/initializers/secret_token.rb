c309182c9a32093087d05d7f0d14c61513c443fe
tkendalls <director@mathplusacademy.com>
2013-12-06 08:58:42 -0500

    inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mailer::Application.config.secret_token = 'd2c0706a2c0fc76bef26e47b5dd44be599e81a934204dcedd848ec7e4d08c08f972f61ce1cb00315ba54665af81736a244ff12434682a75a9511b6797fcd42fb'
