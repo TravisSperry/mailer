87dbd653ca924cbcac8324b6b8ec931db0dc2eea
tkendalls <director@mathplusacademy.com>
2013-12-26 11:45:50 -0500

    update to email regex

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,7 +3,7 @@ class Contact < ActiveRecord::Base
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
-  validates :email, :format => { :with => %r{.+@.+\..+} }, :allow_blank => false, :presence => true
+  validates :email, :format => { :with => /^\S+@\S+\.\S+$/ }, :allow_blank => false, :presence => true
 
   def name
     first_name + " " + last_name
