348545e5fdc8a1cf53c8ca130e6b014c41a22865
tkendalls <director@mathplusacademy.com>
2014-02-26 15:54:52 -0500

    update

--- config/initializers/postageapp.rb
+++ config/initializers/postageapp.rb
@@ -1,4 +1,4 @@
 PostageApp.configure do |config|
-  config.api_key            = 'HWXKe24ITnHkcga8BFfwKUAM5cnNIAwG'
+  config.api_key            = 'w3CH8Skb6A1usfe1IeogVWWMER19TLTR'
   config.recipient_override = 'tkendalls1@gmail.com' unless Rails.env.production?
 end
