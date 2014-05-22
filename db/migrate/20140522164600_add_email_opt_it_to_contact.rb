24325e8927d407fe4fc7cf87c32d38b969129132
tkendalls <director@mathplusacademy.com>
2014-05-22 13:37:26 -0400

    update

--- db/migrate/20140522164600_add_email_opt_it_to_contact.rb
+++ db/migrate/20140522164600_add_email_opt_it_to_contact.rb
@@ -1,5 +1,5 @@
 class AddEmailOptItToContact < ActiveRecord::Migration
   def change
-    add_column :contacts, :email_opt_in, :booelan, default: true
+    add_column :contacts, :email_opt_in, :boolean, default: true
   end
 end
