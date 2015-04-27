564649369893163118ad698afadaf05b0c54fe1c
tkendalls <director@mathplusacademy.com>
2015-04-27 11:39:26 -0400

    update

--- app/mailers/pony_express.rb
+++ app/mailers/pony_express.rb
@@ -54,6 +54,14 @@ class PonyExpress < PostageApp::Mailer
       )
   end
 
+  def jrmf_comming(contact_email)
+    attachments['jrmf_flyer_2015.pdf'] = File.read('app/assets/forms/jrmf_flyer_2015.pdf')
+    mail(
+      subject: "The Julia Robinson Mathematics Festival is coming!",
+      to: "#{contact_email}"
+      )
+  end
+
   def vendor_registration_confirmation(vendor_registration)
     @vendor_registration = vendor_registration
     mail(
