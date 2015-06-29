39745663e7bfd6882b6eab7c8a41b7d011f30ac6
tkendalls <director@mathplusacademy.com>
2015-06-29 12:08:25 -0400

    update

--- app/mailers/pony_express.rb
+++ app/mailers/pony_express.rb
@@ -16,7 +16,7 @@ class PonyExpress < PostageApp::Mailer
     @contact = contact
 
     mail(
-      subject: "Enrich Columbus: Monthly Events for June!",
+      subject: "Enrich Columbus: Monthly Events for July!",
       to: "#{contact.email}"
       )
   end
